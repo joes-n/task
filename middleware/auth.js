b07a3e412d9df91f55c6e65ca01efdcd7771ea1b
joes-n <borsen1218@gmail.com>
2025-11-21 05:19:43 +0800

    fix: Fix connect-flash not installed

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,6 @@ module.exports = {
     if (req.session.userId) {
       return next();
     } else {
-      req.flash('error', 'Please log in to access this page');
       return res.redirect('/login');
     }
   },
